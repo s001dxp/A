1a4892b5b2df4248e8c057a78501482b125b834d
Darryl Porter <dlporter98@gmail.com>
2018-03-05 17:10:01 -0500

    Added tests

--- tests/ATest.php
+++ tests/ATest.php
@@ -2,20 +2,72 @@
 namespace Sdxp;
 
 use PHPUnit\Framework\TestCase;
+use Sdxp\A;
 
 class ATest extends TestCase
 {
 
+	public function numberedArrayProvider()
+	{
+		return ['a', 'b', 'c', 'd', 'e', 'f'];
+	}
+
+	public function assocArrayProvider()
+	{
+		return ['a' => 'Neo', 'b' => 'Morpheus', 'c' => 'Trinity', 'd' => 'Smith', 'e' => 'Cypher', 'f' => 'Persephone'];
+	}
+
+	public function getAObjectAssoc()
+	{
+		return A::array($this->assocArrayProvider());
+	}
+
+	public function getAObjectNumerical()
+	{
+		return A::array($this->numberedArrayProvider());
+	}
+
+	public function testConstructWithNumericalArray()
+	{
+		$a = new A($this->numberedArrayProvider());
+		$this->assertInstanceOf('Sdxp\A', $a);
+
+		$this->assertEquals($this->numberedArrayProvider(), $a->getArray());
+	}
+
+	public function testConstructWithAssocArray()
+	{
+		$a = new A($this->assocArrayProvider());
+		$this->assertInstanceOf('Sdxp\A', $a);
+
+		$this->assertEquals($this->assocArrayProvider(), $a->getArray());
+	}
+
+	public function testConstructWithOutArray()
+	{
+		$a = new A();
+		$this->assertInstanceOf('Sdxp\A', $a);
+
+		$this->assertEquals([], $a->getArray());
+	}
+
 	public function testArray()
 	{
-
+		$a = A::array($this->numberedArrayProvider());
+		$this->assertInstanceOf('Sdxp\A', $a);
 	}
 
 	public function testChangeKeyCase()
 	{
+		$a = $this->getAObjectAssoc();
 
+		$b = $a->changeKeyCase(\CASE_UPPER);
+		$this->assertNotEquals(\array_keys($b->getArray()), \array_keys($this->assocArrayProvider()));
+
+		$c = $b->changeKeyCase();
+		$this->assertEquals(\array_keys($c->getArray()), \array_keys($this->assocArrayProvider()));
 	}
-
+/*
 	public function testChunk()
 	{
 
@@ -214,7 +266,7 @@ class ATest extends TestCase
 	public function testShuffle()
 	{
 
-	}
+	}*/
 
 	/**
 	 * Call protected/private method of a class.
